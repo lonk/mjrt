6ad3d5b71a51a525213acb77634365da2c87dd6c
Lucas Soulier <lucas@buckless.com>
2020-04-16 01:24:46 +0200

    fix(notify): fix notifications on mobile

--- web/src/components/Notify/Notify.tsx
+++ web/src/components/Notify/Notify.tsx
@@ -4,9 +4,18 @@ import Notification from 'react-web-notification';
 export default function Notify(props: any) {
     const [isGranted, setIsGranted] = useState(false);
     const [notificationShown, setNotificationShown] = useState(true);
+    const [registration, setRegistration] = useState<
+        ServiceWorkerRegistration
+    >();
     const audio = new Audio('./notify.mp3');
 
     useEffect(() => {
+        // Will force re-render due to the previous setNotificationShown === false
+        setNotificationShown(true);
+    });
+
+    useEffect(() => {
+        registerSw();
         audio.load();
     }, []);
 
@@ -17,26 +26,33 @@ export default function Notify(props: any) {
         }
     }, [props.notify]);
 
-    const check = () => {
-        setNotificationShown(true);
+    const registerSw = async () => {
+        const navigatorRegistration = await navigator.serviceWorker.register(
+            'sw.js'
+        );
+        setRegistration(navigatorRegistration);
     };
 
     return (
-        <Notification
-            ignore={!isGranted || notificationShown}
-            askAgain={true}
-            onPermissionGranted={() => setIsGranted(true)}
-            onShow={() => check()}
-            timeout={5000}
-            title="Mjrt.net"
-            options={{
-                tag: Date.now().toString(),
-                body: 'La partie est sur le point de commencer',
-                icon:
-                    'http://mobilusoss.github.io/react-web-notification/example/Notifications_button_24.png',
-                lang: 'fr',
-                dir: 'ltr'
-            }}
-        />
+        <div>
+            {Boolean(registration) && (
+                <Notification
+                    ignore={!isGranted || notificationShown}
+                    askAgain={true}
+                    onPermissionGranted={() => setIsGranted(true)}
+                    timeout={5000}
+                    title="Mjrt.net"
+                    options={{
+                        tag: Date.now().toString(),
+                        body: 'La partie est sur le point de commencer',
+                        icon:
+                            'http://mobilusoss.github.io/react-web-notification/example/Notifications_button_24.png',
+                        lang: 'fr',
+                        dir: 'ltr'
+                    }}
+                    swRegistration={registration}
+                />
+            )}
+        </div>
     );
 }
