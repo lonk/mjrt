de4bd0a258b79ebae445fd94b7010dda740abef4
Lucas Soulier <lucas@buckless.com>
2020-04-27 18:16:30 +0200

    fix(web): handle unmatched routes

--- web/src/views/App/App.tsx
+++ web/src/views/App/App.tsx
@@ -1,31 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch
+} from 'react-router-dom';
 import Login from '../Login/Login';
 import Play from '../Play/Play';
-import styles from  './App.module.css';
+import styles from './App.module.css';
 
 export default function App() {
     return (
-        <Router>
-            <div className={styles.app}>
-                <Route exact path="/">
+        <div className={styles.app}>
+            <Router>
+                <Switch>
+                    <Route exact path="/login/:id?">
+                        <Login />
+                    </Route>
+                    <Route exact path="/play/:id?">
+                        <Play />
+                    </Route>
                     <Redirect to="/login" />
-                </Route>
-                <Route exact path="/login/:id?">
-                    <Login />
-                </Route>
-                <Route exact path="/play/:id?">
-                    <Play />
-                </Route>
-                <div className={styles.motionTwin}>
-                    Ce jeu est librement inspiré du génial{' '}
-                    <a href="http://majority.muxxu.com">Majority</a> de la
-                    Motion Twin !
-                    <br />
-                    N'hésitez pas à contribuer sur{' '}
-                    <a href="https://github.com/lonk/mjrt">GitHub</a> !
-                </div>
+                </Switch>
+            </Router>
+            <div className={styles.motionTwin}>
+                Ce jeu est librement inspiré du génial{' '}
+                <a href="http://majority.muxxu.com">Majority</a> de la Motion
+                Twin !
+                <br />
+                N'hésitez pas à contribuer sur{' '}
+                <a href="https://github.com/lonk/mjrt">GitHub</a> !
             </div>
-        </Router>
+        </div>
     );
 }
